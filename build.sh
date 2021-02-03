98a2dadcaae31dcf72afe56ae6b3cd97420e3426
Tomas Slavik <tomas.slavik@microsoft.com>
2021-02-03 16:51:52 +0100

    all perm added

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ BLUE='\033[0;34m'
 NC='\033[0m'
 
 printf "=== ${BLUE}Building %s sample${NC} ===\n" "${PWD##*/}"
-sudo ./compile.sh && ./compress.sh && ./test.sh
+sudo . compile.sh && sudo . compress.sh && sudo . test.sh
