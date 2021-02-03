335e266a71ea1d394807aaccb7509e88ac704eca
Tomas Slavik <tomas.slavik@microsoft.com>
2021-02-03 16:57:12 +0100

    bash sudo

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ BLUE='\033[0;34m'
 NC='\033[0m'
 
 printf "=== ${BLUE}Building %s sample${NC} ===\n" "${PWD##*/}"
-sudo . compile.sh && sudo . compress.sh && sudo . test.sh
+sudo bash compile.sh && sudo bash compress.sh && sudo bash test.sh
